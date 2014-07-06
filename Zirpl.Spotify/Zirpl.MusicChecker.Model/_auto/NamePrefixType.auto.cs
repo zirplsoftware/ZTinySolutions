db06dc2247fd86e62345d035322470e3cfa4bef6
nlafratta <nlafratta@zirpl.com>
2014-07-06 17:33:26 -0400

    working on correctly generating the files (split is not working)

--- Zirpl.Spotify/Zirpl.MusicChecker.Model/_auto/NamePrefixType.auto.cs
+++ Zirpl.Spotify/Zirpl.MusicChecker.Model/_auto/NamePrefixType.auto.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Zirpl.AppEngine.Model;
-
-namespace Zirpl.MusicChecker.Model
-{
-    public partial class NamePrefixType : DictionaryEntityBase<byte, NamePrefixTypeEnum>
-    {
-		public override string Name { get; set; }
-    }
-}
-
+﻿
--- Zirpl.Spotify/Zirpl.MusicChecker.Model/_auto/NameSuffixType.auto.cs
+++ Zirpl.Spotify/Zirpl.MusicChecker.Model/_auto/NameSuffixType.auto.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Zirpl.AppEngine.Model;
-
-namespace Zirpl.MusicChecker.Model
-{
-    public partial class NameSuffixType : DictionaryEntityBase<byte, NameSuffixTypeEnum>
-    {
-		public override string Name { get; set; }
-    }
-}
-
+﻿
