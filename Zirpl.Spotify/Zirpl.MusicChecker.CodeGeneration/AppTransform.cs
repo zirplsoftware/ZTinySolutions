294d8677911d82fe703c550dee24f834dba3902e
nlafratta <nlafratta@zirpl.com>
2014-07-06 22:49:54 -0400

    reorganizing logic to better encapulate app generation

--- Zirpl.Spotify/Zirpl.MusicChecker.CodeGeneration/AppTransform.cs
+++ Zirpl.Spotify/Zirpl.MusicChecker.CodeGeneration/AppTransform.cs
@@ -3,6 +3,14 @@
 // Zirpl.MusicChecker.Model\_auto\Settings\AppTransform.txt4
 // Zirpl.MusicChecker.Model\_auto\AppTransform.txt4
 // Generated items
+// Zirpl.MusicChecker.Model\_auto\Settings\StateProvinceTypeEnum.auto.cs
+// Zirpl.MusicChecker.Model\_auto\NameSuffixTypeEnum.auto.cs
+// Zirpl.MusicChecker.Model\_auto\NamePrefixTypeEnum.auto.cs
+// Zirpl.MusicChecker.Model\_auto\Settings\SystemSettingMetadata.auto.cs
+// Zirpl.MusicChecker.Model\_auto\Settings\StateProvinceTypeMetadata.auto.cs
+// Zirpl.MusicChecker.Model\_auto\NameSuffixTypeMetadata.auto.cs
+// Zirpl.MusicChecker.Model\_auto\NamePrefixTypeMetadata.auto.cs
+// Zirpl.MusicChecker.Model\_auto\AddressMetadata.auto.cs
 // Zirpl.MusicChecker.Model\_auto\Settings\SystemSetting.auto.cs
 // Zirpl.MusicChecker.Model\_auto\Settings\StateProvinceType.auto.cs
 // Zirpl.MusicChecker.Model\_auto\NameSuffixType.auto.cs
---
+++ Zirpl.Spotify/Zirpl.MusicChecker.Model/_auto/NamePrefixTypeEnum.auto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Zirpl.AppEngine.Model;
+
+namespace Zirpl.MusicChecker.Model
+{
+    public enum NamePrefixTypeEnum : byte
+    {
+        Dr = 1,
+        Ms = 2,
+        Mrs = 3,
+        Mr = 4
+	}
+}
