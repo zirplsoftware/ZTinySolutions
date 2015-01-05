673beade17a98621111d1575702c904342cb7fc2
Nathan LaFratta <nlafratta@zirpl.com>
2015-01-05 05:15:40 -0500

    cleaning up the bug proof to it's simplest state. static readonly fields are handled diffferently in x64 vs x32 bit versions

--- Zirpl.ProveBug/Zirpl.ProveBug/Program.cs
+++ Zirpl.ProveBug/Zirpl.ProveBug/Program.cs
@@ -5,45 +5,27 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zirpl.ProvdeBug
+namespace Zirpl.ProveBug
 {
     public class Program
     {
-        public class A { }
-        public class B : A { }
-        public class C : B { }
-        public class Mock
-        {
-            public B publicField;
-            private B privateField;
-            protected B protectedField;
-            internal B internalField;
-            protected internal B protectedInternalField;
-
-            public static B publicStaticField;
-            private static B privateStaticField;
-            protected static B protectedStaticField;
-            internal static B internalStaticField;
-            protected internal static B protectedInternalStaticField;
-
-            public static readonly int readonlyStaticInt;
-            public readonly int readonlyInt;
-        }
-        public class MockDerived : Mock
-        {
-
-        }
+        public static readonly int readonlyStaticInt;
+        public static readonly String readonlyStaticString;
+        public static readonly bool readonlyStaticBool;
 
         static void Main(string[] args)
         {
-            new Program().test();
-        }
-
-        public void test()
-        {
-            Console.WriteLine(Mock.readonlyStaticInt);
-            typeof(Mock).GetField("readonlyStaticInt", BindingFlags.Public | BindingFlags.Static).SetValue(null, 2);
-            Console.WriteLine(Mock.readonlyStaticInt);
+            Console.WriteLine("Initial value of readonlyStaticInt: " + Program.readonlyStaticInt);
+            typeof(Program).GetField("readonlyStaticInt", BindingFlags.Public | BindingFlags.Static).SetValue(null, 2);
+            Console.WriteLine("New value of readonlyStaticInt: " + Program.readonlyStaticInt);
+            Console.WriteLine();
+            Console.WriteLine("Initial value of readonlyStaticString: " + Program.readonlyStaticString);
+            typeof(Program).GetField("readonlyStaticString", BindingFlags.Public | BindingFlags.Static).SetValue(null, 2.ToString());
+            Console.WriteLine("New value of readonlyStaticString: " + Program.readonlyStaticString);
+            Console.WriteLine();
+            Console.WriteLine("Initial value of readonlyStaticBool: " + Program.readonlyStaticBool);
+            typeof(Program).GetField("readonlyStaticBool", BindingFlags.Public | BindingFlags.Static).SetValue(null, true);
+            Console.WriteLine("New value of readonlyStaticBool: " + Program.readonlyStaticBool);
 
             Console.ReadKey();
         }
